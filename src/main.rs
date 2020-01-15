e0db7ef5fdb3a900a8df217ec675585c3d471147
Alex Kinnane <akinnane@yahoo.co.uk>
2020-01-15 23:52:44 +0000

    Working code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
-extern crate clap;
-extern crate reqwest;
-use clap::{App, Arg};
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::error::Error;
 use std::path::Path;
+use clap::{App, Arg};
+use itertools::Itertools;
 
-fn lines_from_file<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::new("efflux")
         .version("v1.0-beta")
         .arg(
@@ -54,35 +46,81 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .long("sourcetype")
                 .value_name("sourcetype")
                 .help("The splunk sourcetype")
-                .default_value("text"),
+                .default_value("generic_single_line"),
         )
         .get_matches();
 
-    let file = matches.value_of("file").unwrap();
-    let token = matches.value_of("token").unwrap();
+    let client = reqwest::blocking::Client::new();
+
     let host = matches.value_of("host").unwrap();
     let source = matches.value_of("source").unwrap();
     let sourcetype = matches.value_of("sourcetype").unwrap();
-
     let url = format!(
         "https://{}/services/collector/raw?source={}&sourcetype={}",
         host, source, sourcetype
     );
 
+    let token = matches.value_of("token").unwrap();
     let auth = format!("Splunk {}", token);
 
-    let client = reqwest::Client::new();
+    let file = matches.value_of("file").unwrap();
 
-    for (index, line) in lines_from_file(file).unwrap().enumerate() {
-        let line = line.unwrap();
+    for (n, lines) in lines_from_file(file)?
+        .batching(|it| batch_lines(it))
+        .enumerate()
+    {
+        let size = lines.len();
         let resp = client
             .post(&url)
-            .body(line.clone())
+            .body(lines)
             .header("Authorization", &auth)
             .send();
 
-        println!("{:#?}:{}:{}", resp.unwrap().status(), index + 1, line);
+        println!(
+            "request:{:?} size:{:#?} status:{:#?}",
+            n,
+            size,
+            resp.unwrap().status()
+        );
     }
 
     Ok(())
+}
+
+fn lines_from_file<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn batch_lines<I>(it: &mut I) -> Option<String>
+where
+    I: Iterator<Item = std::result::Result<String, io::Error>>,
+    I: std::fmt::Debug,
+{
+    let max = 1024 * 950;
+    let mut lines = String::with_capacity(max);
+
+    let mut size = 0;
+    while size < max {
+        match it.next() {
+            None => {
+                break;
+            }
+            Some(x) => {
+                let s = x.unwrap();
+                size += s.len();
+                lines.push_str(s.as_str());
+                lines.push('\n');
+            }
+        }
+    }
+
+    if lines.is_empty() {
+        None
+    } else {
+        Some(lines)
+    }
 }
